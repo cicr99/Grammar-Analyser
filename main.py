184eef0459b8909c8e2980fbfcf2c50820b74449
cicr <carmen.cabrera@estudiantes.matcom.uh.cu>
2020-02-15 10:15:52 -0500

    visual app running, ready to receive an input

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 from input import GrammarFromInput
 from cmp.tools import compute_firsts, compute_follows
 from pprint import pprint
+import streamlit as st
 
-text = '''
-Distinguido = <e, 'e'>
-NoTerminales = [<x, 'x'>, <y, 'y'>, <z, 'z'>]
-Terminales = [<a, 'b'>, <c, 'd'>]
-e = x
-x = y + z + a
-y = a + z; a + c
-z = c
-'''
+
+# text = '''
+# Distinguido = <e, 'e'>
+# NoTerminales = [<x, 'x'>, <y, 'y'>, <z, 'z'>]
+# Terminales = [<a, 'b'>, <c, 'd'>]
+# e = x
+# x = y + z + a
+# y = a + z; a + c
+# z = c
+# '''
 
 if __name__ == '__main__':
-    G = GrammarFromInput(text).GiveGrammar()
-    print(G)
+    st.title('Grammar Analyser')
 
-    firsts = compute_firsts(G)
-    pprint(firsts)
-    follows = compute_follows(G, firsts)
-    print()
-    pprint(follows)
-    
+    st.sidebar.markdown('''Produced by:  
+    Carmen Irene Cabrera Rodríguez  
+    Enrique Martínez González''')
+
+    text = st.text_area('Input your grammar here:')
+
+    if text:
+        G = GrammarFromInput(text).GiveGrammar()
+        # print(G)
+        st.write(G)
+        firsts = compute_firsts(G)
+        follows = compute_follows(G, firsts)
+
+
+
---
+++ test.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+a = {'mama': 52, 'papa' : 50}
+
+st.write(a)
